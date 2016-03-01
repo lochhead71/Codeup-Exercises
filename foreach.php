f01f2b56acf3b52302f752834af2db32c81af2a4
Jim Lochhead <lochhead71@gmail.com>
2016-03-01 10:36:22 -0600

    Finished step four of PHP foreach loop exercise - nested foreach loops!

--- foreach.php
+++ foreach.php
@@ -3,7 +3,12 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $datatypes) {
-	if (is_scalar($datatypes)) {
-		echo "{$datatypes} has only one value.\n";
+	if (is_array($datatypes)) {
+		echo gettype($datatypes) . PHP_EOL;
+		foreach ($datatypes as $items) {
+			echo $items . PHP_EOL;
+		}
+	} else {
+		echo $datatypes . PHP_EOL;
 	}
 }
