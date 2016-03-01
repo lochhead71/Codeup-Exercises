b576e93ddb2f2cf7ab2d33a5f3db385f31d98f62
Jim Lochhead <lochhead71@gmail.com>
2016-03-01 15:17:03 -0600

    Added Modulus Function

--- arithmetic.php
+++ arithmetic.php
@@ -20,7 +20,13 @@ function divide($a, $b)
     return $a / $b;
 }
 
+function modulus($a, $b)
+{
+	return $a % $b;
+}
+
 echo add(5, 9) . PHP_EOL;
 echo subtract(45, 23) . PHP_EOL;
 echo multiply(3, 6) . PHP_EOL;
 echo divide(63, 9) . PHP_EOL;
+echo modulus(64, 7) . PHP_EOL;
