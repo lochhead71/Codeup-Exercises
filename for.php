15ad8d7cd3def880bfb16f210315c7a4918a4033
Jim Lochhead <lochhead71@gmail.com>
2016-03-01 11:57:19 -0600

    added last feature ensuring integer values

--- for.php
+++ for.php
@@ -1,15 +1,19 @@
 <?php
 
-fwrite(STDOUT, "Please type an integer: ");
+while ($startNum == 0) {
+	fwrite(STDOUT, "Please type an integer: ");
 	$startNum = (int)trim(fgets(STDIN));
+}
 
-fwrite(STDOUT, "Please type another: ");
+while ($startNum > $endNum) {
+	fwrite(STDOUT, "Please enter an integer greater than the first value.\n");
 	$endNum = (int)trim(fgets(STDIN));
+}
 
 fwrite(STDOUT, "Choose an increment value: ");
 	$incrValue = (int)trim(fgets(STDIN));
 
-if ((bool)$incrValue == false) {
+if ($incrValue == 0) {
 	$incrValue = 1;
 }
 
