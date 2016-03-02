3c5bab1b6c55d8dc481b3f0a4bb4b1d70edcdb15
Jim Lochhead <lochhead71@gmail.com>
2016-03-02 13:05:03 -0600

    Last commit for arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -1,44 +1,60 @@
 <?php
 
+function throwErrorMessage($var1, $var2, $error = 'numberError') {
+	if ($error == 'numberError') {
+		return "ERROR: Both arguments must be numbers; \$a is a " . gettype($var1) . " and \$b is a " . gettype($var2) . ".";		
+	} elseif ($error == 'divisorError') {
+		return "ERROR: You can't divide by zero! Your divisor is " . $var2;
+	}
+}
+
+function numericInputs ($x, $y) {
+    if (is_numeric($x) && is_numeric($y)) {
+    	return true;
+	} else {
+		return false;
+	}
+}
+
 function add($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (numericInputs ($a, $b)) {
         return $a + $b;
     } else {
-        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
+        return throwErrorMessage($a, $b);
     }
 }
 
 function subtract($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (numericInputs ($a, $b)) {
 		return $a - $b;
 	} else {
-        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
+        return throwErrorMessage($a, $b);
     }
 }
 
 function multiply($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (numericInputs ($a, $b)) {
 		return $a * $b;
 	} else {
-        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
+        return throwErrorMessage($a, $b);
     }
 }
 
 function divide($a, $b) {
-    if (is_numeric($a) && is_numeric($b) && $b != 0){
+    if (numericInputs ($a, $b) && $b != 0){
 		return $a / $b;
 	} elseif ($b == 0) {
-		return "ERROR: You can't divide by zero!\n";
+		return "ERROR: You can't divide by zero!";
 	} else {
-        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
+        return throwErrorMessage($a, $b, 'divisorError');
     }
 }
 
 function modulus($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (numericInputs ($a, $b)) {
 		return $a % $b;
 	} else {
-        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
+        return throwErrorMessage($a, $b, 'divisorError');
     }
 }
 
