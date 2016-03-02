022097473d6a8dd5c52070c2d90fdab5797597b0
Jim Lochhead <lochhead71@gmail.com>
2016-03-02 09:10:03 -0600

    Fixed divide by zero message and included variable values in error message

--- arithmetic.php
+++ arithmetic.php
@@ -1,13 +1,10 @@
 <?php
 
-$a = 7;
-$b = 9;
-
 function add($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
         return $a + $b;
     } else {
-        return "ERROR: Both arguments must be numbers\n";
+        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
     }
 }
 
@@ -15,7 +12,7 @@ function subtract($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
 		return $a - $b;
 	} else {
-        return "ERROR: Both arguments must be numbers\n";
+        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
     }
 }
 
@@ -23,30 +20,30 @@ function multiply($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
 		return $a * $b;
 	} else {
-        return "ERROR: Both arguments must be numbers\n";
+        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
     }
 }
 
 function divide($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
+    if (is_numeric($a) && is_numeric($b) && $b != 0){
 		return $a / $b;
+	} elseif ($b == 0) {
+		return "ERROR: You can't divide by zero!\n";
 	} else {
-        return "ERROR: Both arguments must be numbers\n";
+        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
     }
 }
 
 function modulus($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) && $b != 0{
+    if (is_numeric($a) && is_numeric($b)) {
 		return $a % $b;
-	} elseif ($b == 0)
-		return "ERROR: You can\'t divide by zero!\n"
-	 else {
-        return "ERROR: Both arguments must be numbers\n";
+	} else {
+        return "ERROR: Both arguments must be numbers; \$a is {$a} and \$b is {$b}.\n";
     }
 }
 
 echo add(5, 9) . PHP_EOL;
-echo subtract(45, 23) . PHP_EOL;
+echo subtract(45, 'twenty-three') . PHP_EOL;
 echo multiply(3, 6) . PHP_EOL;
-echo divide(63, 9) . PHP_EOL;
+echo divide(63, 0) . PHP_EOL;
 echo modulus(64, 7) . PHP_EOL;
