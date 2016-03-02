90e76ded1663814021f409fddfe6f11a6aeba848
Jim Lochhead <lochhead71@gmail.com>
2016-03-02 09:00:03 -0600

    Validated arguements and created special case for dividing by zero

--- arithmetic.php
+++ arithmetic.php
@@ -3,29 +3,46 @@
 $a = 7;
 $b = 9;
 
-function add($a, $b)
-{
-    return $a + $b;
+function add($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a + $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-function subtract($a, $b)
-{
-    return $a - $b;
+function subtract($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) {
+		return $a - $b;
+	} else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-function multiply($a, $b)
-{
-    return $a * $b;
+function multiply($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) {
+		return $a * $b;
+	} else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-function divide($a, $b)
-{
-    return $a / $b;
+function divide($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) {
+		return $a / $b;
+	} else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-function modulus($a, $b)
-{
-	return $a % $b;
+function modulus($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) && $b != 0{
+		return $a % $b;
+	} elseif ($b == 0)
+		return "ERROR: You can\'t divide by zero!\n"
+	 else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 echo add(5, 9) . PHP_EOL;
