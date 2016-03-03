b2296d97d9495c86ce52483853d0af047f6c3696
Jim Lochhead <lochhead71@gmail.com>
2016-03-03 13:11:37 -0600

    fixed bug in bonus questions

--- merge-arrays.php
+++ merge-arrays.php
@@ -4,18 +4,32 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
+$moreNames = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael', 'John'];
+
+$evenMoreNames = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam', 'Joey', 'Sam', 'Pete'];
+
 function combineArrays($firstArray, $secondArray) {
 	$mergedArrays = [];
-	foreach($firstArray as $key => $name) {
-		$result = array_search($name, $secondArray);
-		if ($firstArray[$key] == $secondArray[$result]) {
-			$mergedArrays[] = $firstArray[$result];
-		} elseif ($result === false) {
-			$mergedArrays[] = $firstArray[$key];
-			$mergedArrays[] = $secondArray[$key];
+	if (count($firstArray) > count($secondArray)) {
+		$longerArray = $firstArray;
+		$shorterArray = $secondArray;
+	} else {
+		$longerArray = $secondArray;
+		$shorterArray = $firstArray;
+	}
+	foreach($longerArray as $key => $name) {
+		if (array_key_exists($key, $shorterArray) && $longerArray[$key] == $shorterArray[$key]) {
+			$mergedArrays[] = $longerArray[$key];
+		} else {
+			$mergedArrays[] = $longerArray[$key];
+			if  (array_key_exists($key, $shorterArray)) {
+				$mergedArrays[] = $shorterArray[$key];
+			}
 		}
 	}
 	return $mergedArrays;
 }
 
-print_r(combineArrays($names, $compare));
+print_r(combineArrays($names, $compare)) . PHP_EOL;
+
+print_r(combineArrays($moreNames, $evenMoreNames));
