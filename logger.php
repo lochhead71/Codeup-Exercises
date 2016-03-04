30b349f826bf7035aa6b7c8de45afd062a07d0c1
Jim Lochhead <lochhead71@gmail.com>
2016-03-04 13:59:35 -0600

    completed step two creating the logMessage function

--- logger.php
+++ logger.php
@@ -3,6 +3,11 @@
 function logMessage($logLevel, $message)
 {
     // todo - complete this function
+$filename = "log-" . date('Y-m-d') . ".log";
+$handle = fopen($filename, 'a');
+$log = date('Y-m-d G:i:s') . "[" . $logLevel . "] " . $message . PHP_EOL;
+fwrite($handle, $log);
+fclose($handle);
 }
 
 logMessage("INFO", "This is an info message.");
